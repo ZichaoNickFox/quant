e8032ec0361352175e6652e9d691b4c12dbcd9b7
zichaonickfox <zichaonickfox@gmail.com>
2025-12-08 16:04:21 +0800

    Remove ihp

--- ihp/dev/Paths_ihp_ide.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -Wno-deprecations  #-}
-module Paths_ihp_ide where
-
-import Data.Version
-import Prelude
-
-version = Version { versionBranch = [1, 3, 0], versionTags = [] }
-
-getDataFileName path = pure path
